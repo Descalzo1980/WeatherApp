6eb37efb1ecbee7ea0b39933e34270e4868bd9bf
Leonov Stanislav <ragioniere1980@gmail.com>
2024-05-04 11:55:48 +0300

    add data

---
+++ app/src/main/java/dev/stas/weatherapp/data/network/dto/ForecastDto.kt
@@ -0,0 +1,8 @@
+package dev.stas.weatherapp.data.network.dto
+
+import com.google.gson.annotations.SerializedName
+
+data class ForecastDto(
+    @SerializedName("forecastday")
+    val forecastDay : List<DayDto>
+)
