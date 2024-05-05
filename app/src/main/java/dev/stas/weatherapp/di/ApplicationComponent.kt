ed0125dbfe7a8fe570022cf36013ba94e857130b
Leonov Stanislav <ragioniere1980@gmail.com>
2024-05-05 12:46:45 +0300

    impl FavoriteStore

--- app/src/main/java/dev/stas/weatherapp/di/ApplicationComponent.kt
+++ app/src/main/java/dev/stas/weatherapp/di/ApplicationComponent.kt
@@ -3,10 +3,12 @@ package dev.stas.weatherapp.di
 import android.content.Context
 import dagger.BindsInstance
 import dagger.Component
+
 @ApplicationScope
 @Component(
     modules = [
-        DataModule::class
+        DataModule::class,
+        PresentationModule::class
     ]
 )
 interface ApplicationComponent {
